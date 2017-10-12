e5d5c50c64eeb16d7e803dbf5ba9e570d799da26
Doraemon <1224500763@qq.com>
2017-10-12 11:21:08 +0800

    g

---
+++ home/js/doraemon1.js
@@ -0,0 +1,66 @@
+/**
+ * Created by Mr.zhao on 2017/10/11.
+ */
+function fun() {
+
+    <!-- 哆啦A梦 -->
+<div class="doa">
+
+        <!-- 头 -->
+
+        <div class="head">
+        <div class="face">
+        <div>
+        <div>
+        <div></div>
+        </div>
+        <div>
+        <div></div>
+        </div>
+        </div>
+        <div>
+        <div>
+        <div></div>
+        </div>
+        </div>
+        </div>
+
+        <div class="nose">
+        <div></div>
+        </div>
+        <div class="nose1"></div>
+        </div>
+
+        <!-- 嘴 -->
+
+        <div class="mouth">
+        <div></div>
+        <div></div>
+        </div>
+
+        <!-- 脖 -->
+
+        <div class="neck">
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        </div>
+
+        <!-- 胡须 -->
+
+        <div class="beard">
+        <div class="left">
+        <div></div>
+        <div></div>
+        <div></div>
+        </div>
+        <div class="right">
+        <div></div>
+        <div></div>
+        <div></div>
+        </div>
+        <span></span>
+        </div>
+        </div>
+}
